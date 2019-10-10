5318963d4e78190c60bf3cf2f88250a63c1f4202
onyeka mmakwe <notmaks@yahoo.com>
2019-10-10 01:50:40 +0100

    create category view

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 import Header from './components/Header/Header';
 import './App.scss';
 import { Switch, Route } from 'react-router-dom';
+import CategoryPage from './pages/CategoryPage/CategoryPage';
 function App() {
   return (
     <div>
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
+        <Route path='/:categoryId' component={CategoryPage} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
---
+++ src/pages/CategoryPage/CategoryPage.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import VerticalCard from '../../components/VerticalCard/VerticalCard';
+
+const CategoryPage = ({ match }) => {
+  console.log(match);
+  return (
+    <div className='container'>
+      <div className='category-section'>
+        <h2 className='category-section__title'>{match.params.categoryId}</h2>
+        <hr />
+        <div className='three-colum-grid'>
+          <VerticalCard />
+          <VerticalCard />
+          <VerticalCard />
+        </div>
+        <div className='view-more'>
+          <button className='btn btn__full'>View More</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryPage;
