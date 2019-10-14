07b97185f9b72b420a30b652823dfbd459b8d7bf
onyeka mmakwe <notmaks@yahoo.com>
2019-10-14 15:41:29 +0100

    Updates

---
+++ precache-manifest.2cd20601debaf5a193d4d2d5319f192d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a150431e3b0854b2e054f58c2f1b762c",
+    "url": "/Random-Guys-Frontend/index.html"
+  },
+  {
+    "revision": "91f1154c8c2641f0b548",
+    "url": "/Random-Guys-Frontend/static/css/main.54702641.chunk.css"
+  },
+  {
+    "revision": "4fdf010a77bab9aaf83e",
+    "url": "/Random-Guys-Frontend/static/js/2.af4e7993.chunk.js"
+  },
+  {
+    "revision": "91f1154c8c2641f0b548",
+    "url": "/Random-Guys-Frontend/static/js/main.d36456f9.chunk.js"
+  },
+  {
+    "revision": "36b13ea2f186a453e8bf",
+    "url": "/Random-Guys-Frontend/static/js/runtime-main.5e196eb1.js"
+  },
+  {
+    "revision": "ff5dc5fa1694b9d154cc085d7dc635c5",
+    "url": "/Random-Guys-Frontend/static/media/notFound.ff5dc5fa.svg"
+  },
+  {
+    "revision": "ad17389da1a937fde5b81a846202c2ba",
+    "url": "/Random-Guys-Frontend/static/media/search.ad17389d.svg"
+  }
+]);
